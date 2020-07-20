ad16306e258b0771e7e830d74f9230cf724fbaca
unknown <hamdibadreldin@gmail.com>
2020-07-20 01:45:58 -0700

    Like and DisLike

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question','QuestionController');
 Route::apiResource('/category','CategoryController');
 Route::apiResource('/reply','ReplyController');
+
+Route::post('/like/{reply}','LikesController@likeIt');
+Route::delete('/like/{reply}','LikesController@unLikeIt');

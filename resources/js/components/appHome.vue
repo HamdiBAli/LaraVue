ec01e2d0bd33e45889b7bd70a753d15eef785a9d
unknown <hamdibadreldin@gmail.com>
2020-08-13 06:09:43 -0700

    Add vue Router NavBar Footer

---
+++ resources/js/components/appHome.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <toolBar></toolBar>
+        <h1>Home</h1>
+        <app-footer></app-footer>
+    </div>
+</template>
+<script>
+import toolBar from './toolBar'
+import appFooter from './footer'
+    export default{
+        components:{toolBar,appFooter}
+    }
+</script>
+<style>
+
+</style>
